740e66163dcf7d9ede28a93e81ce54937a6c044e
rameshswamyp <rameshswamy.ps@gmail.com>
2021-06-25 16:04:14 +0530

    myfirstcomit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+     static  class  Program
+     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            //Program oobj = new Program();
+
+             Console.WriteLine("Hello World!");
+
+             int res= Program .Add(10,20);
+             int i = Program.sub(20,10);
+
+             Console.WriteLine(+res);
+             Console.WriteLine(i);
+        }
+    }
+}
