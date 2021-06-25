f090ed27e0ecb145288b84ccf7a472a63f1e5bac
rameshswamyp <rameshswamy.ps@gmail.com>
2021-06-25 16:33:09 +0530

    mysecondcommit

---
+++ Breakpack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Breakpack_Operations
+{
+    public class Breakpack
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Type
+        {
+            get;
+            set;
+        }
+      
+        public string Name
+        {
+            get;
+            set;
+        }
+
+
+    }
+
+}
